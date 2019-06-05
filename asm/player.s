5c4e5abb9b728889f6317034be6cd042da9dd8d4
Yaroslava HLIBOCHKO <yhliboch@e1r4p11.unit.ua>
2019-06-05 04:20:40 -0700

    need to debug

---
+++ asm/player.s
@@ -0,0 +1,45 @@
+.name		".comment 'privet'"
+.comment	"reufeigfuewgfu"
+
+
+kik:
+live %5
+live %:live
+ld    	%0, r8
+ld  78, r9
+ld  	%:loop, r-2
+st  r9, r10
+st r6, 89
+loop:
+st r7, :pop
+add r9, r8, r7
+sub r4, r3, r6
+and r9, %6, r6
+tip:
+and %8, 8, r9
+and %:pip, :pip, r0
+and 67, 67, r4
+or 67, 67, r4
+or %:pip, :pip, r0
+or %8, 8, r9
+or r9, %6, r6
+xor r9, %6, r6
+pop:
+xor r9, %6, r6
+xor r9, %6, r6
+zjmp %6
+zjmp %:tip
+ldi r9, %897, r78
+ldi 67, %:tip, r87
+ldi :tip, %67, r56
+sti r9, r8, %34
+sti r23, %1, %:kik
+pip:
+sti	r9, :kik, r56
+fork %67
+fork %:loop
+lld %:loop, r9
+lldi :loop, %67, r9
+lfork %567
+live:
+aff r9
